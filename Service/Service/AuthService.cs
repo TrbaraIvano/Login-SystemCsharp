ba4b9b535a4b2cea913b47e33812f2d66e66d3f8
IvanoTrbara <i.trbara@students.htl-leonding.ac.at>
2026-02-23 21:33:29 +0100

    Project referenzen gemacht AuthService angefangen Models fertig

---
+++ Service/Service/AuthService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LoginSystem.Models;
+
+namespace LoginSystem.Service
+{
+    public class AuthService
+    {
+        private List<User> UserList { get; set; } = new List<User>();
+        public string Path { get; } = "users.csv";
+
+    }
+}
